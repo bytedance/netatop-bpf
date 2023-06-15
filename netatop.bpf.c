0fded746692525cb75a256781d2651f42f861405
liuting.0xffff <liuting.0xffff@bytedance.com>
2023-06-15 20:47:34 +0800

    use community kernel's hooks

--- netatop.bpf.c
+++ netatop.bpf.c
@@ -17,12 +17,12 @@ struct {
 	__type(value, struct taskcount);
 } tgid_net_stat SEC(".maps");
 
-struct {
-	__uint(type, BPF_MAP_TYPE_PERCPU_HASH);
-	__uint(max_entries, TASK_MAX_ENTRIES);
-	__type(key, u64);
-	__type(value, struct taskcount);
-} tid_net_stat SEC(".maps");
+// struct {
+// 	__uint(type, BPF_MAP_TYPE_PERCPU_HASH);
+// 	__uint(max_entries, TASK_MAX_ENTRIES);
+// 	__type(key, u64);
+// 	__type(value, struct taskcount);
+// } tid_net_stat SEC(".maps");
 
 static __always_inline u64 current_tgid()
 {
@@ -38,157 +38,106 @@ static __always_inline u64 current_tgid()
 // 	return tid;
 // }
 
-SEC("raw_tracepoint/udp_send_length")
-int BPF_PROG(udp_send_length_k, struct sock *sk, int length, int error, int flags)
-{
-	struct taskcount *stat_tgid, *stat_tid;
+struct sock_msg_length_args {
+	unsigned long long common_tp_fields;
+	struct sock *sk;
+	__u16 family;
+	__u16 protocol;
+	int length;
+	int error;
+	int flags;
+};
 
-	if (error != 0)
-		return 0;
-
-	u64 tgid = current_tgid();
-	stat_tgid = bpf_map_lookup_elem(&tgid_net_stat, &tgid);
-	if (stat_tgid) {
-		stat_tgid->udpsndpacks++;
-		stat_tgid->udpsndbytes += length;
-	} else {
-		struct taskcount data ={
-			.udpsndpacks = 1,
-			.udpsndbytes = length
-		};
-		
-		long ret = bpf_map_update_elem(&tgid_net_stat, &tgid, &data, BPF_ANY);
-	}
-	// u64 tid = current_tid();
-	// stat_tid = bpf_map_lookup_elem(&tid_net_stat, &tid);
-	// if (stat_tid) {
-	// 	stat_tid->udpsndpacks++;
-	// 	stat_tid->udpsndbytes += length;
-	// } else {
-	// 	struct taskcount data ={
-	// 		.udpsndpacks = 1,
-	// 		.udpsndbytes = length
-	// 	};
-		
-	// 	long ret = bpf_map_update_elem(&tid_net_stat, &tid, &data, BPF_ANY);
-	// }
-	return 0;
-}
-
-SEC("raw_tracepoint/udp_recv_length")
-int BPF_PROG(udp_recv_length_k, struct sock *sk, int length, int error, int flags)
-{
-	struct taskcount *stat_tgid, *stat_tid;
-
-	if (error != 0)
-		return 0;
-
-	u64 tgid = current_tgid();
-	stat_tgid = bpf_map_lookup_elem(&tgid_net_stat, &tgid);
-	if (stat_tgid) {
-		stat_tgid->udprcvpacks++;
-		stat_tgid->udprcvbytes += length;
-	} else {
-		struct taskcount data ={
-			.udprcvpacks = 1,
-			.udprcvbytes = length
-		};
-		
-		long ret = bpf_map_update_elem(&tgid_net_stat, &tgid, &data, BPF_ANY);
-	}
-	// u64 tid = current_tid();
-	// stat_tid = bpf_map_lookup_elem(&tid_net_stat, &tid);
-	// if (stat_tid) {
-	// 	stat_tid->udprcvpacks++;
-	// 	stat_tid->udprcvbytes += length;
-	// } else {
-	// 	struct taskcount data ={
-	// 		.udprcvpacks = 1,
-	// 		.udprcvbytes = length
-	// 	};
-		
-	// 	long ret = bpf_map_update_elem(&tid_net_stat, &tid, &data, BPF_ANY);
-	// }
-	return 0;
-}
-
-SEC("raw_tracepoint/tcp_send_length")
-int BPF_PROG(tcp_send_length_k, struct sock *sk, int length, int error, int flags)
-{
-	struct taskcount *stat_tgid,*stat_tid;
-	int id;
-
-	if (error != 0)
-		return 0;
-
-	u64 tgid = current_tgid(); 
-	stat_tgid = bpf_map_lookup_elem(&tgid_net_stat, &tgid);
-	if (stat_tgid) {
-		stat_tgid->tcpsndpacks++;
-		stat_tgid->tcpsndbytes += length;
-	} else {
-		struct taskcount data ={
-			.tcpsndpacks = 1,
-			.tcpsndbytes = length
-		};
-		
-		long ret = bpf_map_update_elem(&tgid_net_stat, &tgid, &data, BPF_ANY);
-	}
-	// u64 tid = current_tid(); 
-	// stat_tid = bpf_map_lookup_elem(&tid_net_stat, &tid);
-	// if (stat_tid) {
-	// 	stat_tid->tcpsndpacks++;
-	// 	stat_tid->tcpsndbytes += length;
-	// } else {
-	// 	struct taskcount data ={
-	// 		.tcpsndpacks = 1,
-	// 		.tcpsndbytes = length
-	// 	};
-		
-	// 	long ret = bpf_map_update_elem(&tid_net_stat, &tid, &data, BPF_ANY);
-	// }
-	return 0;
-}
-
-SEC("raw_tracepoint/tcp_recv_length")
-int BPF_PROG(tcp_recv_length_k, void *sk, int length, int error, int flags)
+SEC("tracepoint/sock/sock_sendmsg_length")
+int handle_tp_send(struct sock_msg_length_args *ctx)
 {
 	struct taskcount *stat_tgid, *stat_tid;
 	int id;
+	short family = ctx->family;
+	short protocol = ctx->protocol;
+	int length = ctx->length;
+	int error = ctx->error;
 
 	if (error != 0)
 		return 0;
 
-	u64 tgid = current_tgid();
-	// u64 tid = current_tid();
-	stat_tgid = bpf_map_lookup_elem(&tgid_net_stat, &tgid);
-	// stat_tid = bpf_map_lookup_elem(&tid_net_stat, &tid);
-
-	if (stat_tgid) {
-		stat_tgid->tcprcvpacks++;
-		stat_tgid->tcprcvbytes += length;
-		// bpf_printk("current_tgidtttttttt %d %d\n", stat_tgid->tcprcvpacks, stat_tgid->tcprcvbytes);
-	} else {
-		struct taskcount data ={
-			.tcprcvpacks = 1,
-			.tcprcvbytes = length
-		};
-		
-		long ret = bpf_map_update_elem(&tgid_net_stat, &tgid, &data, BPF_ANY);
-		// bpf_printk("bpf_map_update_elem %d %d\n", data.tcprcvpacks, data.tcprcvbytes);
+	// AF_INET = 2
+	// AF_INET6 = 10
+	if (family == 2 || family ==  10) {
+		u64 tgid = current_tgid();
+		// u64 tid = current_tid();
+		stat_tgid = bpf_map_lookup_elem(&tgid_net_stat, &tgid);
+		// stat_tid = bpf_map_lookup_elem(&tid_net_stat, &tid);
+		if (protocol == IPPROTO_TCP) {
+			if (stat_tgid) {
+				stat_tgid->tcpsndpacks++;
+				stat_tgid->tcpsndbytes += length;
+			} else {
+				struct taskcount data ={
+					.tcpsndpacks = 1,
+					.tcpsndbytes = length
+				};
+				long ret = bpf_map_update_elem(&tgid_net_stat, &tgid, &data, BPF_ANY);
+			}
+		} else if (protocol == IPPROTO_UDP) {
+			if (stat_tgid) {
+				stat_tgid->udpsndpacks++;
+				stat_tgid->udpsndbytes += length;
+			} else {
+				struct taskcount data ={
+					.udpsndpacks = 1,
+					.udpsndbytes = length
+				};
+				long ret = bpf_map_update_elem(&tgid_net_stat, &tgid, &data, BPF_ANY);
+			}
+		}
 	}
+	return 0;
+}
 
-	// if (stat_tid) {
-	// 	stat_tid->tcprcvpacks++;
-	// 	stat_tid->tcprcvbytes += length;
-	// 	// bpf_printk("current_tidtttttttt %llu %d %d\n", tid, stat_tid->tcprcvpacks, stat_tid->tcprcvbytes);
-	// } else {
-	// 	struct taskcount data ={
-	// 		.tcprcvpacks = 1,
-	// 		.tcprcvbytes = length
-	// 	};
-	// 	long ret = bpf_map_update_elem(&tid_net_stat, &tid, &data, BPF_ANY);
-	// }
+SEC("tracepoint/sock/sock_recvmsg_length")
+int handle_tp_recv(struct sock_msg_length_args *ctx)
+{
+	struct taskcount *stat_tgid, *stat_tid;
+	int id;
+	short family = ctx->family;
+	short protocol = ctx->protocol;
+	int length = ctx->length;
+	int error = ctx->error;
+
+	if (error != 0)
+		return 0;
+	if (family == 2 || family == 10) {
+		u64 tgid = current_tgid();
+		// u64 tid = current_tid();
+		stat_tgid = bpf_map_lookup_elem(&tgid_net_stat, &tgid);
+		// stat_tid = bpf_map_lookup_elem(&tid_net_stat, &tid);
+		if (protocol == IPPROTO_TCP) {
+			if (stat_tgid) {
+				stat_tgid->tcprcvpacks++;
+				stat_tgid->tcprcvbytes += length;
+				// bpf_printk("current_tgidtttttttt %d %d\n", stat_tgid->tcprcvpacks, stat_tgid->tcprcvbytes);
+			} else {
+				struct taskcount data ={
+					.tcprcvpacks = 1,
+					.tcprcvbytes = length
+				};
+				long ret = bpf_map_update_elem(&tgid_net_stat, &tgid, &data, BPF_ANY);
+			}
+		} else if (protocol == IPPROTO_UDP) {
+			if (stat_tgid) {
+				stat_tgid->udprcvpacks++;
+				stat_tgid->udprcvbytes += length;
+				// bpf_printk("current_tgidtttttttt %d %d\n", stat_tgid->tcprcvpacks, stat_tgid->tcprcvbytes);
+			} else {
+				struct taskcount data ={
+					.udprcvpacks = 1,
+					.udprcvbytes = length
+				};
+				long ret = bpf_map_update_elem(&tgid_net_stat, &tgid, &data, BPF_ANY);
+			}
+		}
+	}
 	return 0;
 }
 
