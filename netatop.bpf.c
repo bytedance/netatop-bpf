404327d2debd4190cdcfc81f5388c3490f3c084e
Ting Liu <liuting.0xffff@bytedance.com>
2023-07-27 19:40:16 +0800

    recv no datas

--- netatop.bpf.c
+++ netatop.bpf.c
@@ -56,10 +56,6 @@ int handle_tp_send(struct sock_msg_length_args *ctx)
 	short family = ctx->family;
 	short protocol = ctx->protocol;
 	int length = ctx->length;
-	int error = ctx->error;
-
-	if (error != 0)
-		return 0;
 
 	// AF_INET = 2
 	// AF_INET6 = 10
@@ -103,26 +99,24 @@ int handle_tp_recv(struct sock_msg_length_args *ctx)
 	short family = ctx->family;
 	short protocol = ctx->protocol;
 	int length = ctx->length;
-	int error = ctx->error;
 
-	if (error != 0)
-		return 0;
 	if (family == 2 || family == 10) {
 		u64 tgid = current_tgid();
 		// u64 tid = current_tid();
 		stat_tgid = bpf_map_lookup_elem(&tgid_net_stat, &tgid);
 		// stat_tid = bpf_map_lookup_elem(&tid_net_stat, &tid);
 		if (protocol == IPPROTO_TCP) {
+
 			if (stat_tgid) {
 				stat_tgid->tcprcvpacks++;
 				stat_tgid->tcprcvbytes += length;
-				// bpf_printk("current_tgidtttttttt %d %d\n", stat_tgid->tcprcvpacks, stat_tgid->tcprcvbytes);
 			} else {
 				struct taskcount data ={
 					.tcprcvpacks = 1,
 					.tcprcvbytes = length
 				};
 				long ret = bpf_map_update_elem(&tgid_net_stat, &tgid, &data, BPF_ANY);
+
 			}
 		} else if (protocol == IPPROTO_UDP) {
 			if (stat_tgid) {
